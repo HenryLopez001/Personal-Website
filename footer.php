e614b9f65630cda2f8e2050b0fcf9efd5fa27415
LOPEZ HENRY <150036653@cart.ads>
2019-06-05 12:49:09 -0700

    Final

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "Henry Lopez's Personal Website Email: tailshenry@icloud.com Phone Number: 555-555-5555 Linkedin: Placeholder"
+echo "Henry Lopez's Personal Website Email: tailshenry@icloud.com Phone Number: 555-555-5555 Linkedin: <a href='https://www.linkedin.com/in/henry-lopez-cart/' target='_blank'>LinkedIn</a>"
 ?>
